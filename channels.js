16865536d7613a9ff239dbf322ec462f4724972b
PRENDLYMADAPAKER <palasa1919@gmail.com>
2025-10-17 14:04:28 +0800

    Update channels.js

--- channels.js
+++ channels.js
@@ -10,8 +10,8 @@ const combinedChannels = [
   { name: "4K Travel TV", type: "hls",
     url: "https://streams2.sofast.tv/sofastplayout/33c31ac4-51fa-46ae-afd0-0d1fe5e60a80_0_HLS/master.m3u8" },
   
-  { name: "KBS World (HLS)", type: "hls",
-    url: "https://kbsworld-ott.akamaized.net/hls/live/2002341/kbsworld/master.m3u8" },
+  { name: "Cinemo", type: "hls",
+    url: "https://live-faws.akamaized.net/out/v1/93b9db7b231d45f28f64f29b86dc6c65/index.mpd" },
 
   { name: "ABS-CBN Live (YouTube)", type: "youtube",
     url: "https://www.youtube.com/embed/x_qgWdUGb0s" },
@@ -31,26 +31,83 @@ const combinedChannels = [
     name: "ANC",
     type: "dash",
     url: "https://cdn-ue1-prod.tsv2.amagi.tv/linear/amg01006-abs-cbn-anc-global-dash-abscbnono/index.mpd",
+    category: "Local Channels",
     clearKey: {
       "4bbdc78024a54662854b412d01fafa16": "6039ec9b213aca913821677a28bd78ae"
     }
   },
 
   {
-    name: "A2Z (DASH)",
+    name: "BILYONARYO CHANNEL",
+    type: "dash",
+    url: "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/bilyonaryoch/default1/index.mpd",
+    clearKey: {
+      "227ffaf09bec4a889e0e0988704d52a2": "b2d0dce5c486891997c1c92ddaca2cd2"
+    }
+  },
+
+  {
+    name: "BUKO",
+    type: "dash",
+    url: "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/cg_buko_sd/default/index.mpd",
+    clearKey: {
+      "d273c085f2ab4a248e7bfc375229007d": "7932354c3a84f7fc1b80efa6bcea0615"
+    }
+  },
+
+  {
+    name: "CELESTIAL MOVIES PINOY",
+    type: "dash",
+    url: "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/celmovie_pinoy_sd/default/index.mpd",
+    clearKey: {
+      "0f8537d8412b11edb8780242ac120002": "2ffd7230416150fd5196fd7ea71c36f3"
+    }
+  },
+
+  {
+    name: "A2Z",
     type: "dash",
     url: "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/cg_a2z/default/index.mpd",
+    category: "Local Channels",
     clearKey: {
       "f703e4c8ec9041eeb5028ab4248fa094": "c22f2162e176eee6273a5d0b68d19530"
     }
   },
 
   {
-    name: "GMA Pinoy TV (DASH)",
+    name: "ANC",
     type: "dash",
-    url: "https://cdn-uw2-prod.tsv2.amagi.tv/linear/amg01006-abs-cbn-abscbn-gma-x7-dash-abscbnono/7c693236-e0c1-40a3-8bd0-bb25e43f5bfc/index.mpd",
+    url: "https://cdn-ue1-prod.tsv2.amagi.tv/linear/amg01006-abs-cbn-anc-global-dash-abscbnono/index.mpd",
+    category: "Local Channels",
     clearKey: {
-      "c95ed4c44b0b4f7fa1c6ebbbbaab21a1": "47635b8e885e19f2ccbdff078c207058"
+      "4bbdc78024a54662854b412d01fafa16": "6039ec9b213aca913821677a28bd78ae"
+    }
+  },
+
+  {
+    name: "BILYONARYO CHANNEL",
+    type: "dash",
+    url: "https://qp-pldt-live-bpk-02-prod.akamaized.net/bpk-tv/bilyonaryoch/default1/index.mpd",
+    clearKey: {
+      "227ffaf09bec4a889e0e0988704d52a2": "b2d0dce5c486891997c1c92ddaca2cd2"
+    }
+  },
+
+  {
+    name: "BUKO",
+    type: "dash",
+    url: "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/cg_buko_sd/default/index.mpd",
+    clearKey: {
+      "d273c085f2ab4a248e7bfc375229007d": "7932354c3a84f7fc1b80efa6bcea0615"
+    }
+  },
+
+  {
+    name: "BUKO",
+    type: "dash",
+    url: "https://qp-pldt-live-bpk-01-prod.akamaized.net/bpk-tv/cg_buko_sd/default/index.mpd",
+    clearKey: {
+      "d273c085f2ab4a248e7bfc375229007d": "7932354c3a84f7fc1b80efa6bcea0615"
     }
   },
 ];
